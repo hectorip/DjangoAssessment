ae31b7e3ff5e7ea5b1b81a6c430ef1e058222910
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-03-21 00:07:57 -0600

    moving urls to own module

--- swift_quotes/quotes/tests.py
+++ swift_quotes/quotes/tests.py
@@ -5,6 +5,6 @@ from .views import home_page
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_the_home_page(self):
-        found = resolve('/')
+        found = resolve('/quotes/')
         self.assertEqual(found.func, home_page)
 # Create your tests here.
